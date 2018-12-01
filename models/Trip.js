624f955319fd8b1af8878dcffb4a7cad84d9b953
chauncy-n <eldoon@comcast.net>
2018-11-30 16:40:34 -0800

    built the trip model

--- models/Trip.js
+++ models/Trip.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 
+const ParkSchema = new mongoose.Schema ({
+    parkCode: String
+})
+
 const tripSchema = new mongoose.Schema({
-    
+    name: String,
+    startDate: Date,
+    endDate: Date,
+    parks:[ParkSchema]
 })
 
 module.exports = mongoose.model('Trip', tripSchema);
