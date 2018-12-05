e4f7a7a21490a9877734061c74e78ef548510921
chauncy-n <eldoon@comcast.net>
2018-12-05 10:35:57 -0800

    tweaking server

--- index.js
+++ index.js
@@ -18,22 +18,22 @@ require('./config/database')
 
 app.use(express.static(__dirname + "/client/build"));
 
-var loginLimiter = new RateLimit({
-  windowMs: 5*60*1000, // 5 minutes
-  max: 3,
-  delayMs: 0, // disabled
-  message: "Maximum login attempts exceeded. Please try again later."
-});
+// var loginLimiter = new RateLimit({
+//   windowMs: 5*60*1000, // 5 minutes
+//   max: 3,
+//   delayMs: 0, // disabled
+//   message: "Maximum login attempts exceeded. Please try again later."
+// });
 
-var signupLimiter = new RateLimit({
-  windowMs: 60*60*1000, // 60 minutes
-  max: 3,
-  delayMs: 0, // disabled
-  message: "Maximum accounts created. Please try again later."
-});
+// var signupLimiter = new RateLimit({
+//   windowMs: 60*60*1000, // 60 minutes
+//   max: 3,
+//   delayMs: 0, // disabled
+//   message: "Maximum accounts created. Please try again later."
+// });
 
-app.use('/auth/login', loginLimiter);
-app.use('/auth/signup', signupLimiter);
+// app.use('/auth/login', loginLimiter);
+// app.use('/auth/signup', signupLimiter);
 
 app.use('/api', api)
 app.use('/auth', auth);
